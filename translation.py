7951672dcd4901a82d4c2d5aa8ec2333570e8a38
Pramudithbhashitha <75405197+Pramudithbhashitha@users.noreply.github.com>
2020-12-18 22:58:08 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -43,8 +43,8 @@ Expires on: NO EXPIRED"""
 --------
 Send /me to know current plan details
 
-Support Group : @InFoTelGroup
-© @TGBotsZ"""
+Support Group : @tech_warriors_LK
+© @MATShub"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
